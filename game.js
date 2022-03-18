41995b71e39f54c38095f13f8868addab5cadeda
nocity <nocity24@gmail.com>
2022-03-18 03:01:50 -0700

    console displays who wins each round. bug is not displaying the correct sore at end of game.

--- game.js
+++ game.js
@@ -1,50 +1,65 @@
+
 const button = document.querySelector('button');
 let moveSet = ['rock', 'paper', 'scissors'];
+let points = 0;
 
 //choses random move for the computer
 function computerPlay () {
     let randomIndex = (Math.floor((Math.random()) *(moveSet.length)));
-    let computerChoice = (moveSet[randomIndex]);
-    console.log('Computer:' + computerChoice);
-    
+    console.log('Computer: ' + moveSet[randomIndex]);
+    return moveSet[randomIndex];    
 }
 //prompts the user to input their move
 //does not continue if the input is invalid
 //case insensitive; input transitioned into lowercase by default
 function playerMove () {
     let playerChoice = ((prompt('Rock, paper, or scissors?')).toLowerCase());
-    console.log(playerChoice);
+    console.log("player: " + playerChoice)
         if (playerChoice == 'rock'){
-            console.log('good choice');
         }
-        else if (playerChoice == 'paper'){
-            console.log('good choice');
+        else if (playerChoice == 'paper'){  
         }
         else if (playerChoice == 'scissors'){
-            console.log('good choice');
         }
         else {
             console.log('try again');
             playerMove();
         }
+    return playerChoice;
 }
 //compares the moves of computer and player
 function playRound () {
-    playerMove();
-    computerPlay();
-
+    let playerChoice = playerMove();
+    let computerChoice = computerPlay();
+    let score = 0;
     if (computerChoice == playerChoice) {
         console.log('tie!');
     }
-    if (playerChoice == 'rock' && computerChoice == 'scissors'){
+    else if (playerChoice == 'rock' && computerChoice == 'scissors'){
         console.log('you won!');
+        let points = (+score + 1);
+        return points;
     }
-    if (playerChoice == 'paper' && computerChoice == 'rock'){
+    else if (playerChoice == 'paper' && computerChoice == 'rock'){
         console.log('you won!');
+        let points = (+score + 1);
+        return points;
     }
-    if (playerChoice == 'scissors' && computerChoice == 'paper'){
+    else if (playerChoice == 'scissors' && computerChoice == 'paper'){
     console.log('you won!');
+    let points = (+score + 1);
+    return points;
+    }
+    else {
+        console.log('you lost!');
     }
 }
-//!!! bug: computerPlay is not entering a defined string inside the playRound function
-button.addEventListener('click', playRound);
+//plays a 5-round game
+function game () {
+    let scoreBoard = parseInt(points, 10);
+    for (let i = 0; i < 5; i++) {
+        playRound();
+    }
+    console.log('your score was: ' + +scoreBoard);
+}
+button.addEventListener('click', game); 
