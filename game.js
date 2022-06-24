36c5f868b0f2f799effaae91cb1121de1efc7f51
nocity24 <nocity24@gmail.com>
2022-06-23 23:05:27 -0700

    the program now works, It has some ugly code but it is fully functional.

--- game.js
+++ game.js
@@ -1,50 +1,86 @@
 const button = document.querySelector('button');
 let moveSet = ['rock', 'paper', 'scissors'];
+//creates variables for the scores
+let playerScore = 0;
+let computerScore = 0;
 
 //choses random move for the computer
 function computerPlay () {
     let randomIndex = (Math.floor((Math.random()) *(moveSet.length)));
-    let computerChoice = (moveSet[randomIndex]);
-    console.log('Computer:' + computerChoice);
+    return (moveSet[randomIndex]);
     
 }
 //prompts the user to input their move
 //does not continue if the input is invalid
 //case insensitive; input transitioned into lowercase by default
 function playerMove () {
-    let playerChoice = ((prompt('Rock, paper, or scissors?')).toLowerCase());
+    return ((prompt('Rock, paper, or scissors?')).toLowerCase());
+}
+function playRound () {
+    let playerChoice = playerMove();
+    let computerChoice = computerPlay();
+
     console.log(playerChoice);
-        if (playerChoice == 'rock'){
-            console.log('good choice');
+//gives player feedback
+        if (playerChoice != 'rock'){
         }
-        else if (playerChoice == 'paper'){
-            console.log('good choice');
+        else if (playerChoice != 'paper'){
         }
-        else if (playerChoice == 'scissors'){
-            console.log('good choice');
+        else if (playerChoice != 'scissors'){
         }
         else {
-            console.log('try again');
-            playerMove();
+            console.log('try again, invalid input');
         }
-}
-//compares the moves of computer and player
-function playRound () {
-    playerMove();
-    computerPlay();
+
+        console.log('Computer:' + computerChoice);
+        
 
     if (computerChoice == playerChoice) {
         console.log('tie!');
     }
     if (playerChoice == 'rock' && computerChoice == 'scissors'){
         console.log('you won!');
+        playerScore = (playerScore + 1);
+    }
+    if (playerChoice == 'rock' && computerChoice == 'paper'){
+        console.log('you lost');
+        computerScore = (computerScore + 1);
     }
     if (playerChoice == 'paper' && computerChoice == 'rock'){
         console.log('you won!');
+        playerScore = (playerScore + 1);
+    }
+    if (playerChoice == 'paper' && computerChoice == 'scissors'){
+        console.log('you lost!');
+        computerScore = (computerScore + 1);
     }
     if (playerChoice == 'scissors' && computerChoice == 'paper'){
     console.log('you won!');
+        playerScore = (playerScore + 1);
+    }
+    if (playerChoice == 'scissors' && computerChoice == 'rock'){
+        console.log('you lost!');
+        computerScore = (computerScore + 1);    
+    }
+    return playerScore;
+}
+function game () {
+    for (let i = 0; i < 5; i++) {
+    playRound ();
+    console.log(playerScore);
+    }
+
+    console.log('Player score: ' + playerScore + ' Computer score: ' + computerScore)
+
+    if (playerScore > computerScore){
+        console.log('congragulations! you won');
+    }
+    else if (playerScore < computerScore){
+        console.log('Oh no! you lost');
+    }
+    else if ( playerScore == computerScore){
+        console.log('It was a tie!');
     }
 }
-//!!! bug: computerPlay is not entering a defined string inside the playRound function
-button.addEventListener('click', playRound);
+    
+button.addEventListener('click', game);
